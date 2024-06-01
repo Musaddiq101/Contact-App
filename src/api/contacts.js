ca76270c7d2dbb71e5307931df21fb775fed36c6
Musaddiq101 <mbhutto34@gmail.com>
2024-06-02 03:29:38 +0800

    restore to prev state

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const apiUrl ="https://my-json-server.typicode.com/Musaddiq101/Json-serve-contacts";
-
 export default axios.create({
-    baseURL: apiUrl,
+    baseURL: "http://localhost:3006/",
 })
